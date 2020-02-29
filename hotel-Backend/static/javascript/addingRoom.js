db7a0479e109f530ba8138f3ff02930c34826705
mahas500 <10518579@mydbs.ie>
2020-02-29 19:42:40 +0000

    session id added in add room page and delete room page for backend authentication

--- hotel-Backend/static/javascript/addingRoom.js
+++ hotel-Backend/static/javascript/addingRoom.js
@@ -3,10 +3,16 @@ $("document").ready(function()
 
     $("#sendingData").click(function()
     {
+        var head = $("#userSession").val();
         var roomnumber = $("#number").val();
         var roomprice = $("#roomprice").val();
         var ratings = $("#ratings").val();
         $.post({url: "http://127.0.0.1:5000/addRoom",
+
+            headers: {
+        session_id:head
+    },
+
            data: JSON.stringify({room_number: roomnumber,price:roomprice,ratingOutofTen:ratings}),
             contentType: "application/json",
             success: function()
--- hotel-Backend/static/javascript/deleteRoom.js
+++ hotel-Backend/static/javascript/deleteRoom.js
@@ -3,13 +3,17 @@ $("document").ready(function()
 
     $("#sendingData").click(function()
     {
+        var head = $("#userSession").val();
         var roomID = $("#number").val();
         $.post({url: "http://127.0.0.1:5000/deleteRoom",
+            headers: {
+        session_id:head
+    },
            data: JSON.stringify({room_id: roomID}),
             contentType: "application/json",
-            success: function()
+            success: function(result)
             {
-                console.log("deleted!!!")
+                console.log(result)
 
             }});
 
